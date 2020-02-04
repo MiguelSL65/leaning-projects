63ac217eb29f65cec3ee5e08faf35163d988d685
Miguel Lemos <mimiguelsl65@gmail.com>
2020-02-04 18:57:24 +0000

    simple gphx

---
+++ simple-graphics/src/org/academiadecodigo/simplegraphics/Main.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.simplegraphics;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(10, 10, 200, 300);
+        rectangle.setColor(Color.RED);
+        rectangle.fill();
+
+        Picture pic = new Picture(0, 150);
+        pic.draw();
+    }
+
+
+}
